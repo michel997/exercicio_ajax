4adf47c606c41e2bc7afd5d22de8173d4b435138
michel997 <gekhanom@outlook.com>
2023-07-06 10:41:29 -0300

    requisições de nome,username,url,img

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-        const endpoint = `https://api.github.com/users/ogiansouza`
+        const user = 'michel997'
+        const endpoint = `https://api.github.com/users/${user}`
 
         fetch(endpoint)
         .then(function (resposta) {
@@ -9,9 +10,17 @@ $(document).ready(function() {
             const followers = json.followers
             const follow = json.following
             const repo = json.public_repos
+            const url = json.html_url
+            const name = json.name
+            const username = json.login
+            const image = json.avatar_url
 
             $('#followers').html(followers);
             $('#follow').html(follow);
             $('#repo').html(repo);
+            $('#name').html(name);
+            $('#user').html(username)
+            $('#github_link').attr('href', url);
+            $('#img').attr('src', image)
         })
 })
